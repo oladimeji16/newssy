6f57736839f27034c2d34a3a981c4bb3136c6e51
Oladimeji Mustapha <49591168+oladimeji16@users.noreply.github.com>
2020-11-03 12:13:52 +0100

    added css

--- src/components/NewsArticle.js
+++ src/components/NewsArticle.js
@@ -3,11 +3,12 @@ import React from 'react'
 const NewsArticle = ({ data }) => {
     return (
         <div className="news">
-            <h1>{data.title}</h1>
-            <p>{data.description}</p>
-            <span>{data.author}</span>
-            <span>{data.publishedAt}</span>
-            <span>{data.source}</span>
+            <h1 className='news-title'>{data.title}</h1>
+            <p className='news-desc'>{data.description}</p>
+            <span className='news-author'>{data.author}</span>
+            <span className='news-published'>{data.publishedAt}</span>
+            <br/>
+            <span className='news-source'>{data.source.name}</span>
         </div>
     )
 }
